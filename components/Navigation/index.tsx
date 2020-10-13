35c8a33b5d9455d0f2ba7e3c0525cda4ba000340
endorphin82 <endorphin82@gmail.com>
2020-10-13 18:57:48 +0300

    init

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,97 @@
+import Link from "next/link";
+
+export function Navigation() {
+  return (
+    <nav className="header__menu">
+      <div className="header__logo">
+        <Link href={'/'}>
+          <a>
+            <img src="/assets/imgs/header-logo.png" alt="Full Cycle Digital Agency"/>
+          </a>
+        </Link>
+      </div>
+
+      <ul>
+        <li>
+          <a href="#">services</a>
+          <ul>
+            <li>
+              <Link href={'/development'}>
+                <a>magento development</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/custom-web-development'}>
+                <a>custom web development</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/security-services'}>
+                <a>security services</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="#">partnerships</a>
+          <ul>
+            <li>
+              <Link href={'/outsourcing'}>
+                <a>outsourcing</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/outstaffing'}>
+                <a>outstaffing</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="#">company</a>
+          <ul>
+            <li>
+              <Link href={'/about'}>
+                <a>about</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/contacts'}>
+                <a>contacts</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/about/testi'}>
+                <a>testimonials</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="#">faq</a>
+          <ul>
+            <li>
+              <Link href={'/general'}>
+                <a>general</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/magento-2'}>
+                <a>magento 2</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <Link href={'/portfolio'}>
+            <a>portfolio</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/Home.module.css'
+import {MainLayout} from "../components/MainLayout";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <MainLayout>
+        content
+      </MainLayout>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
