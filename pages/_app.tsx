a4f1e9fd660b605ec8e508af07d96b39875f4f2f
endorphin82 <endorphin82@gmail.com>
2020-10-21 13:09:53 +0300

    add antd form get a quote

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import {Provider} from 'react-redux'
 import {store} from "../redux/store";
 
 import '../styles/globals.scss'
-
+import 'antd/dist/antd.css';
 
 function MyApp({Component, pageProps}) {
   return (
