9064753217803c937690659d5108e97fb90c41e5
endorphin82 <endorphin82@gmail.com>
2020-10-29 13:22:03 +0200

    fix styles for page-content.scss

--- pages/magento-development/index.tsx
+++ pages/magento-development/index.tsx
@@ -10,98 +10,98 @@ export default function Page() {
         <div className="left-sidebar">
 
           <div id="post-4324" className="page post-4324 type-page status-publish hentry">
-          {/*  <p/>*/}
             <p className="custom-text-os">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quality development is the key to a
               successful business project. E-commerce business has become more technologically sophisticated, online
               shopping is gaining a growing audience. Modern stores must meet the ever-growing expectations of
               customers, such as high performance, security, and flexibility.</p>
-            {/*<p></p>*/}
-            <img className="alignnone size-full"
-                    src="/assets/imgs/magento-development/magento.png" alt="magento"/>
-              <p className="custom-text-os">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We have enough experience to accomplish
-                any kinds of requirements. Deep understanding of the Magento architecture allows us to extend platform
-                for
-                different queries including marketplaces and mixed systems.</p>
-              <h4 className="custom-h4">Our services include the following:</h4>
-              <ul id="ul-li-arrow" className="fa-ul custom-icon-ul">
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="/migration/">Migration</a> 1.x - 2.x</p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="/creating-new-store-scratch/">Creating new
-                    store from scratch</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/design/">Custom Design</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/technical-support/">Ongoing support</a> for
-                    the existing projects</p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/extending-extensions/">Extending
-                    extensions</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/3rd-party-integration/">3rd party
-                    integration</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/amazon-ebay-synchronizations/">Amazon, eBay
-                    synchronizations</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/server-configuration/">Server
-                    configuration</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/optimization-frontend-backend-side/">Optimization
-                    (frontend/backend) side</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a
-                    href="http://mintegr.loc/installation-configuration-search-engines-solr-sphinx/">Installation and
-                    configuration of search engines (Solr, Sphinx)</a></p>
-                </li>
-                <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
-                  <p className="custom-text-os"><a href="http://mintegr.loc/seo-optimization/">SEO optimization</a></p>
-                </li>
-              </ul>
-              <p>&nbsp;</p>
-              <h4 className="custom-h4">We offer:</h4>
-              <ul className="fa-ul custom-icon-ul">
-                <li><i className="fa-li fa fa-bar-chart fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">Attractive pricing / performance / quality</p>
-                </li>
-                <li><i className="fa-li fa fa-users fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">10+ highly qualified Magento backend &amp; front-end Developers</p>
-                </li>
-                <li><i className="fa-li fa fa-calendar fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">Accurate deadlines</p>
-                </li>
-                <li><i className="fa-li fa fa-unlock-alt fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">We comply with all the conditions of the NDA, 100% security for your
-                    business</p>
-                </li>
-                <li><i className="fa-li fa fa-thumbs-up fa-thumbs-o-up fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">Continuous and transparent work process</p>
-                </li>
-                <li><i className="fa-li fa fa-reply-all fa-lg" aria-hidden="true"></i>
-                  <p className="custom-text-os">Daily reports on the various channels of your choice (e-mail, chat,
-                    telephone, Skype)</p>
-                </li>
-              </ul>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p className="custom-text-os upper"><b>We offer future owners of online stores to start your business with
-                the best platform that includes the latest trends in e-commerce in the whole world and as result, you
-                will
-                get fast, advanced, competitive and a secure store</b>
-              </p>
+            <p>
+              <img className="alignnone size-full"
+                   src="/assets/imgs/magento-development/magento.png" alt="magento"/>
+            </p>
+            <p className="custom-text-os">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We have enough experience to accomplish
+              any kinds of requirements. Deep understanding of the Magento architecture allows us to extend platform
+              for
+              different queries including marketplaces and mixed systems.</p>
+            <h4 className="custom-h4">Our services include the following:</h4>
+            <ul id="ul-li-arrow" className="fa-ul custom-icon-ul">
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="/migration/">Migration</a> 1.x - 2.x</p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="/creating-new-store-scratch/">Creating new
+                  store from scratch</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/design/">Custom Design</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/technical-support/">Ongoing support</a> for
+                  the existing projects</p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/extending-extensions/">Extending
+                  extensions</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/3rd-party-integration/">3rd party
+                  integration</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/amazon-ebay-synchronizations/">Amazon, eBay
+                  synchronizations</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/server-configuration/">Server
+                  configuration</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/optimization-frontend-backend-side/">Optimization
+                  (frontend/backend) side</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a
+                  href="http://mintegr.loc/installation-configuration-search-engines-solr-sphinx/">Installation and
+                  configuration of search engines (Solr, Sphinx)</a></p>
+              </li>
+              <li><i className="fa-li fa fa-angle-right" aria-hidden="true"></i>
+                <p className="custom-text-os"><a href="http://mintegr.loc/seo-optimization/">SEO optimization</a></p>
+              </li>
+            </ul>
+            <p>&nbsp;</p>
+            <h4 className="custom-h4">We offer:</h4>
+            <ul className="fa-ul custom-icon-ul">
+              <li><i className="fa-li fa fa-bar-chart fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">Attractive pricing / performance / quality</p>
+              </li>
+              <li><i className="fa-li fa fa-users fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">10+ highly qualified Magento backend &amp; front-end Developers</p>
+              </li>
+              <li><i className="fa-li fa fa-calendar fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">Accurate deadlines</p>
+              </li>
+              <li><i className="fa-li fa fa-unlock-alt fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">We comply with all the conditions of the NDA, 100% security for your
+                  business</p>
+              </li>
+              <li><i className="fa-li fa fa-thumbs-up fa-thumbs-o-up fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">Continuous and transparent work process</p>
+              </li>
+              <li><i className="fa-li fa fa-reply-all fa-lg" aria-hidden="true"></i>
+                <p className="custom-text-os">Daily reports on the various channels of your choice (e-mail, chat,
+                  telephone, Skype)</p>
+              </li>
+            </ul>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p className="custom-text-os upper"><b>We offer future owners of online stores to start your business with
+              the best platform that includes the latest trends in e-commerce in the whole world and as result, you
+              will
+              get fast, advanced, competitive and a secure store</b>
+            </p>
           </div>
         </div>
         <RightSidebar/>
       </div>
     </MainLayout>
-)
+  )
 }
